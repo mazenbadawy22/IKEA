b00766985b8e9d8a3647abac745525b9792879cb
mazenbadawy22 <mazenbbadawy2003@gmail.com>
2025-03-12 03:40:09 +0200

    MVC6

--- IKEA.DAL/Presistance/Repositories/Employees/EmployeeRepository.cs
+++ IKEA.DAL/Presistance/Repositories/Employees/EmployeeRepository.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using IKEA.DAL.Models.Departments;
+using IKEA.DAL.Models.Employees;
 using IKEA.DAL.Presistance.Data;
 using IKEA.DAL.Presistance.Repositories._Generic;
 using Microsoft.EntityFrameworkCore;
 
-namespace IKEA.DAL.Presistance.Repositories.Departments
+namespace IKEA.DAL.Presistance.Repositories.Employees
 {
-    public class DepartmentRepository : GenericRepository<Department>,IDepartmentRepository
+    public class EmployeeRepository:GenericRepository<Employee>,IEmployeeRepository
     {
-       public DepartmentRepository(ApplicationDbContext dbContext) : base(dbContext)
+        public EmployeeRepository(ApplicationDbContext dbContext) : base(dbContext)
         {
 
         }
--- IKEA.DAL/Presistance/Repositories/Employees/IEmployeeRepository.cs
+++ IKEA.DAL/Presistance/Repositories/Employees/IEmployeeRepository.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using IKEA.DAL.Models.Departments;
+using IKEA.DAL.Models.Employees;
 using IKEA.DAL.Presistance.Repositories._Generic;
 
-namespace IKEA.DAL.Presistance.Repositories.Departments
+namespace IKEA.DAL.Presistance.Repositories.Employees
 {
-    public interface IDepartmentRepository:IGenericRepository<Department>
+    public interface IEmployeeRepository:IGenericRepository<Employee>
     {
         
     }
